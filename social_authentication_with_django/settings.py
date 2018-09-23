13aad428d0b3da4f9b3e2da09fb300c30f254a1b
AsadLiam <asadullah.itcgcs@gmail.com>
2018-09-23 09:57:19 +0500

    email removed

--- social_authentication_with_django/settings.py
+++ social_authentication_with_django/settings.py
@@ -138,8 +138,8 @@ STATICFILES_DIRS = [
 
 # sending emails with django
 
-EMAIL_HOST = "smtp.mail.yahoo.com"
-EMAIL_HOST_USER = "asadullah.itcgcs@yahoo.com"
-EMAIL_HOST_PASSWORD = "dasa.55511160"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+# EMAIL_HOST = "smtp.mail.yahoo.com"
+# EMAIL_HOST_USER = ""
+# EMAIL_HOST_PASSWORD = ""
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
