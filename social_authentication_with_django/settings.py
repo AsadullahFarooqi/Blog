71d5e48b051ed042602de33986a3a19bea237cf5
Asadullah Farooqi (Asad) <asadullah.itcgcs@gmail.com>
2018-09-26 20:57:16 +0500

    Update settings.py
    secret key removed

--- social_authentication_with_django/settings.py
+++ social_authentication_with_django/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ms#5il!3pt2g=a6t6v0%xb*_j!o)h9&=8y7@p+kd$9to1s8+v4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
